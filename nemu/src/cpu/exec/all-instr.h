5b1992f28e3b48b274a675524a70f63947abc561
tracer-ics2017 <tracer@njuics.org>
2026-04-29 11:57:26 -0700

    >  compile 2310724 ding Linux ubuntu 4.13.0-36-generic #40~16.04.1-Ubuntu SMP Fri Feb 16 23:26:51 UTC 2018 i686 athlon i686 GNU/Linux  11:57:26 up 2 days, 11:59,  2 users,  load average: 0.00, 0.00, 0.00 b9686c6eaeba1fa58e64c57783415586372218c

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -13,3 +13,18 @@ make_EHelper(operand_size);
 make_EHelper(inv);
 make_EHelper(nemu_trap);
 
+make_EHelper(add);
+make_EHelper(sub);
+make_EHelper(cmp);
+make_EHelper(inc);
+make_EHelper(dec);
+make_EHelper(neg);
+make_EHelper(adc);
+make_EHelper(sbb);
+make_EHelper(mul);
+make_EHelper(imul1);
+make_EHelper(imul2);
+make_EHelper(imul3);
+make_EHelper(div);
+make_EHelper(idiv);
+
