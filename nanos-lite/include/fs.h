e1706113753e39e628b0fba8c9ddc6d1e49d784a
tracer-ics2017 <tracer@njuics.org>
2026-05-24 11:39:10 -0700

    >  run 2310724 ding Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  11:39:10 up 1 day, 19:27,  3 users,  load average: 0.00, 0.00, 0.00 8c193307cb8a63a5ec1e5be722ace7a5d1585bd6

--- nanos-lite/include/fs.h
+++ nanos-lite/include/fs.h
@@ -3,18 +3,18 @@
 
 #include "common.h"
 
-// enum {SEEK_SET, SEEK_CUR, SEEK_END};
+enum {SEEK_SET, SEEK_CUR, SEEK_END};
 
-#ifndef SEEK_SET
-#define SEEK_SET 0
-#endif
+// #ifndef SEEK_SET
+// #define SEEK_SET 0
+// #endif
 
-#ifndef SEEK_CUR
-#define SEEK_CUR 1
-#endif
+// #ifndef SEEK_CUR
+// #define SEEK_CUR 1
+// #endif
 
-#ifndef SEEK_END
-#define SEEK_END 2
-#endif
+// #ifndef SEEK_END
+// #define SEEK_END 2
+// #endif
 
 #endif
