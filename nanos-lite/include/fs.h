87bcc4280982cc6078f04132114625512ec9abba
tracer-ics2017 <tracer@njuics.org>
2026-05-24 11:46:01 -0700

    >  run 2310724 ding Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  11:46:01 up 1 day, 19:34,  3 users,  load average: 0.00, 0.00, 0.00 e40379eb75439903c77310f66f912a9a62e88072

--- nanos-lite/include/fs.h
+++ nanos-lite/include/fs.h
@@ -5,16 +5,4 @@
 
 enum {SEEK_SET, SEEK_CUR, SEEK_END};
 
-// #ifndef SEEK_SET
-// #define SEEK_SET 0
-// #endif
-
-// #ifndef SEEK_CUR
-// #define SEEK_CUR 1
-// #endif
-
-// #ifndef SEEK_END
-// #define SEEK_END 2
-// #endif
-
 #endif
--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -2,7 +2,16 @@
 
 #define DEFAULT_ENTRY ((void *)0x4000000)
 
+void ramdisk_read(void *buf, off_t offset, size_t len);
+size_t get_ramdisk_size(void);
+
 uintptr_t loader(_Protect *as, const char *filename) {
-  TODO();
+  size_t size = get_ramdisk_size();
+
+  Log("load program: ramdisk size = %d bytes", size);
+  Log("load program: entry = %p", DEFAULT_ENTRY);
+
+  ramdisk_read(DEFAULT_ENTRY, 0, size);
+
   return (uintptr_t)DEFAULT_ENTRY;
 }
