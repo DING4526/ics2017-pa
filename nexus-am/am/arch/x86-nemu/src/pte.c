1242f0c1571134cc3b1d385fa63e5a2bc3bd5b2c
tracer-ics2017 <tracer@njuics.org>
2026-05-25 16:41:14 -0700

    >  run 2310724 ding Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  16:41:14 up 2 days, 16:11,  2 users,  load average: 0.00, 0.00, 0.00 d8b3559497c55a8a448800771430aa47b8ece16

--- nexus-am/am/arch/x86-nemu/src/pte.c
+++ nexus-am/am/arch/x86-nemu/src/pte.c
@@ -101,15 +101,13 @@ void _unmap(_Protect *p, void *va) {
 }
 
 _RegSet *_umake(_Protect *p, _Area ustack, _Area kstack, void *entry, char *const argv[], char *const envp[]) {
-  _switch(p);
-
-  _RegSet *tf = (_RegSet *)((uintptr_t)ustack.end - sizeof(_RegSet));
+  _RegSet *tf = (_RegSet *)((uintptr_t)kstack.end - sizeof(_RegSet));
   memset(tf, 0, sizeof(_RegSet));
 
   tf->eip = (uintptr_t)entry;
   tf->cs = KSEL(SEG_KCODE);
   tf->eflags = 0x2;
-  tf->esp = (uintptr_t)ustack.end;
+  tf->esp = (uintptr_t)ustack.end - 32;
 
   tf->irq = 0;
   tf->error_code = 0;
