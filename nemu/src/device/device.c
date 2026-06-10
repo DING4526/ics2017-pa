a1cd40d91b3a07279b25988a10698097a693657e
tracer-ics2017 <tracer@njuics.org>
2026-06-10 02:49:45 -0700

    >  compile 2310724 ding Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  02:49:45 up  2:57,  2 users,  load average: 0.02, 0.14, 0.18 a757c7fa7e678cbd8f44d14143ae3a10f9f6a682

--- nemu/src/device/device.c
+++ nemu/src/device/device.c
@@ -43,11 +43,6 @@ bool device_update_pending() {
 
 void device_update() {
 
-  if (!device_update_flag) {
-    return;
-  }
-  device_update_flag = false;
-
   SDL_Event event;
   while (SDL_PollEvent(&event)) {
     switch (event.type) {
@@ -67,6 +62,12 @@ void device_update() {
         break;
     }
   }
+
+  
+  if (!device_update_flag) {
+    return;
+  }
+  device_update_flag = false;
 
   if (update_screen_flag) {
     update_screen();
