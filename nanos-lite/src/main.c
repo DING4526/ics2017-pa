4f092e93023bcf020d5561caabdf8c7a5197ab32
tracer-ics2017 <tracer@njuics.org>
2026-06-16 10:47:20 -0700

    >  run 2310724 ding Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 athlon i686 GNU/Linux  10:47:20 up 23 min,  3 users,  load average: 0.38, 0.39, 0.22 788d7ffd8f618eeb22f8b57eb4419be2520dcbf1

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -34,9 +34,8 @@ int main() {
   // uint32_t entry = loader(NULL, "/bin/dummy");
   // ((void (*)(void))entry)();
 
-  // load_prog("/bin/hello");
-  // load_prog("/bin/typing");
   load_prog("/bin/pal");
+  load_prog("/bin/hello");
 
   _trap();
 
